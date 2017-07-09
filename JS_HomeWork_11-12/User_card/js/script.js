f2b409c084f9db51c4d3ae2f26a860fd93c02969
Masha Zhovner <masha.zhovner@gmail.com>
2017-07-09 21:33:50 +0300

    added homework_11-12

---
+++ JS_HomeWork_11-12/User_card/js/script.js
@@ -0,0 +1,37 @@
+(function() {
+    'use strict';
+
+    var menuData = {
+            name: 'Жовнер Мария Сергеевна',
+            position: 'Инженер службы балансов электроенергии',
+            reasons: {
+                title: 'Хочу учить фронтенд, потому что:',
+                items: ['Хочу освоить более интересную професию', 'Есть много свободного времени', 'Платят мало']
+            },
+            contacts: {
+                mobileTitle: 'Мой контактный телефон:',
+                mobile: '+38 (095) 749-49-54',
+                linkTitle: 'Мой профиль, вконтакте:',
+                link: 'vk.com/masha.zhovner'
+            },
+            mobile: '+38 (095) 749-49-54',
+            link: '#',
+            feedback: {
+                title: 'Мой фидбек:',
+                text: 'Если нужно, могу свести Вам фидерный баланс'
+            }
+
+            // title: 'Хочу учить фронтенд, потому что:',
+            // items: ['Хочу освоить более интересную професию', 'Есть много свободного времени', 'Платят мало']
+        },
+
+        parent = document.getElementById('container');
+
+
+    var source = document.getElementById('cv').textContent,
+        template = _.template(source, {data: menuData});
+
+    parent.innerHTML += template(menuData);
+
+})();
+
